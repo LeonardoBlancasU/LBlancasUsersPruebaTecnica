1821ec13083eba2efe483482e0a7c0ef41ddfbb2
Leonardo Blancas Uribe <leonardobu28@outlook.com>
2025-08-09 17:47:07 -0600

    CRUD Orders Terminado

---
+++ ML/Orders.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Orders
+    {
+        public int IdOrder { get; set; }
+
+        //public ML.User User { get; set; }
+        //public ML.Truck Truck { get; set; }
+        //public ML.Location PIckUp { get; set; }
+        //public ML.Location DropOff { get; set; }
+        public ML.Status Status { get; set; }
+        public List<object> Ordenes { get; set; }
+    }
+}
---
+++ SL_WebApi/Controllers/OrdersController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SL_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : Controller
+    {
+        private readonly BL.Orders _BLOrders;
+        public OrdersController(BL.Orders BLOrders)
+        {
+            _BLOrders = BLOrders;
+        }
+        [Authorize]
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            ML.Result result = _BLOrders.GetAll();
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [Authorize]
+        [HttpGet]
+        [Route("GetById")]
+        public IActionResult GetById(int IdOrder)
+        {
+            ML.Result result = _BLOrders.GetById(IdOrder);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [Authorize]
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Add([FromBody] ML.Orders order)
+        {
+            ML.Result result = _BLOrders.Add(order);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [Authorize]
+        [HttpPut]
+        [Route("Update")]
+        public IActionResult Update([FromBody] ML.Orders order)
+        {
+            ML.Result result = _BLOrders.Update(order);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [Authorize]
+        [HttpDelete]
+        [Route("Delete")]
+        public IActionResult Delete(int IdOrder)
+        {
+            ML.Result result = _BLOrders.Delete(IdOrder);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [HttpDelete]
+        [Route("UpdateStatus")]
+        public IActionResult UpdateStatus(int IdOrder, int IdStatus)
+        {
+            ML.Result result = _BLOrders.UpdateStatus(IdOrder, IdStatus);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+    }
+}
